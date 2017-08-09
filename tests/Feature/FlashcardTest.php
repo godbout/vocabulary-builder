7b1da11e3157a08e94daf759421a3e0146d8bc53
Guillaume Leclerc <guill.bout@gmail.com>
2017-08-09 22:35:22 +0800

    Update flashcard tests, make them better!

--- tests/Feature/FlashcardTest.php
+++ tests/Feature/FlashcardTest.php
@@ -10,7 +10,7 @@ class FlashcardTest extends TestCase
     {
         parent::setUp();
 
-        $this->artisan('db:seed');
+        $this->artisan('db:seed', ['--class' => 'UsersTableSeeder']);
     }
 
     /**
@@ -19,9 +19,20 @@ class FlashcardTest extends TestCase
      */
     public function guests_can_only_receive_demo_words()
     {
-        /**
-         * TODO
-         */
+        $demoWord = factory(App\Word::class)->create([
+            'user_id'  => 1,
+            'mastered' => false,
+        ]);
+
+        $user = factory(App\User::class)->create();
+        factory(App\Word::class)->create([
+            'user_id' => $user->id,
+        ], 100);
+
+        for ($i = 0; $i < 20; $i++) {
+            $this->get('/flashcards')
+                ->assertSee($demoWord->spelling);
+        }
     }
 
     /**
@@ -30,6 +41,11 @@ class FlashcardTest extends TestCase
      */
     public function authenticated_users_can_only_receive_their_own_words()
     {
+        factory(App\Word::class)->create([
+            'user_id' => 1,
+            'mastered' => false,
+        ], 100);
+        
         $this->be($user = factory(App\User::class)->create());
 
         $word = factory(App\Word::class)->create([
