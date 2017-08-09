cb71f2369060ddb47d420315738347aad1f6b494
Guillaume Leclerc <guill.bout@gmail.com>
2017-08-09 19:03:11 +0800

    Add a noword template for when a user is not owning any word

---
+++ resources/views/words/_nowords.blade.php
@@ -0,0 +1,5 @@
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <h1 class="nowords">No words yet? Create some <a href="{{ url('/words/create') }}">now</a>.</h1>
+    </div>
+</div>
