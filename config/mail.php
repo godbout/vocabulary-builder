be199c115c7ade3cdf15dffab7b01243fba893fb
Guillaume Leclerc <guill.bout@gmail.com>
2017-08-19 18:42:58 +0800

    Update mail settings

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'guill@sleeplessmind.com.mo',
+        'name' => 'Guill',
     ],
 
     /*
