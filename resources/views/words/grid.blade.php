57e7000d0c9f9c096612523173cb78a49e0b561b
Guillaume Leclerc <guill.bout@gmail.com>
2017-09-03 14:14:33 +0800

    Close #16. Improve layout for tablets/phones.

--- resources/views/words/grid.blade.php
+++ resources/views/words/grid.blade.php
@@ -6,7 +6,7 @@
     <div class="container suck-top">
         <div class="row">
             @forelse ($words as $word)
-                <div class="col-md-3">
+                <div class="col-md-3 col-sm-4">
                     <h1>
                         <a @if($word->mastered == true) class="text-success" @endif href="{{ url($word->path()) }}">{{ $word->spelling }}</a>
                     </h1>
