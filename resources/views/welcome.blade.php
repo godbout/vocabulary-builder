6b1e6d06e1e2062ce852db188ee25931db434c3e
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2025-11-08 18:44:11 +0800

    update welcome page title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>v-b — because learn some more</title>
 
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
